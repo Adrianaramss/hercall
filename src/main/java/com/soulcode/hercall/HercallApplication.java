3cc6208d3e61ed8b0e4db018fb50a9aff9908564
Adriana <adrianascosta9@gmail.com>
2024-05-03 19:15:30 -0300

    Initial commit

---
+++ src/main/java/com/soulcode/hercall/HercallApplication.java
@@ -0,0 +1,13 @@
+package com.soulcode.hercall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HercallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HercallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soulcode/hercall/HercallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulcode.hercall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HercallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
